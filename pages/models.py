45a069431037659ced2682c58a280570426f0639
Gokulsidd <92292508+Gokulsidd@users.noreply.github.com>
2023-02-10 22:05:33 +0530

    modified views for displaying the data

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class MedicalCondition(models.Model):
+    record_id = models.CharField(max_length=100)
+    data = models.DateTimeField()
+    diagnosis = models.CharField(max_length=300)
+    
+    
+    def __str__(self):
+        return self.record_id
--- pages/views.py
+++ pages/views.py
@@ -1,22 +1,43 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-import requests , json
+from .models import MedicalCondition
+import requests
 
 
 def HomePageView(request):
-    url = 'http://hapi.fhir.org/baseR4/Condition'
+    url = "http://hapi.fhir.org/baseR4/Condition?_pretty=true"
     response = requests.get(url)
-    db = response.json()
-    return render(request , "home.html" , {'db': db})
+    data = response.json()
+    data_list = []
+    data_id_list = []
+    data_last_updated_list = []
+    data_display_list = []
+    for item in data["entry"]:
+        data_list.append(item)
+    for item in data_list:
+        data_id_list.append(item["resource"]["id"])
+        data_last_updated_list.append(item["resource"]["meta"]["lastUpdated"])
+        data_display_list.append(item["resource"]["code"]["coding"][0]["display"])
+
+    result_data_list = list(
+        zip(data_id_list, data_last_updated_list, data_display_list)
+    )
+
+    return render(
+        request,
+        "home.html",
+        {'db' : result_data_list},
+    )
 
 
 def CreatePageView(request):
-    return render(request , 'create_data.html')
+    return render(request, "create_data.html")
 
- 
-def EditPageView(request):
-    return render(request , "edit_data.html")
- 
-        
+
+def EditPageView(request , id):
+    record = MedicalCondition.objects.get(record_id = id)
+    return render(request, "edit_data.html")
+
+
 def DeletePageView(request):
-    return render(request , 'delete_data.html')
+    return render(request, "delete_data.html")
