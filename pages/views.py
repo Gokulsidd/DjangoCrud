76fe4d9429761d4acf27e404e3447d85448a4b24
Gokulsidd <92292508+Gokulsidd@users.noreply.github.com>
2023-02-15 21:13:21 +0530

    modified for crud operation

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+import json
 from django.views.generic import TemplateView
 from .models import MedicalCondition
 import requests
 
 
 def HomePageView(request):
-    url = "http://hapi.fhir.org/baseR4/Condition?_pretty=true"
+    url = "http://hapi.fhir.org/baseR4/Condition"
     response = requests.get(url)
     data = response.json()
     data_list = []
@@ -31,13 +32,49 @@ def HomePageView(request):
 
 
 def CreatePageView(request):
-    return render(request, "create_data.html")
+    url = "http://hapi.fhir.org/baseR4/Condition"  
+    if request.method == 'POST':
+        display = request.POST.get('display')
+        payload =  {"display": display}
+        condition_json = json.dumps(payload)
+        headers = {"Content-Type": "application/fhir+json"}
+        response = requests.post(url, headers=headers, data=condition_json)
+        temp_data = response.json()
+        print(temp_data)
+        if response.status_code == 201:
+            print("Condition resource created successfully")
+        else:
+            print("Error creating  resource: ", response.text)
+    return render(request, 'create_data.html')
 
 
-def EditPageView(request , id):
-    record = MedicalCondition.objects.get(record_id = id)
+def EditPageView(request):
+    url = "http://hapi.fhir.org/baseR4/Condition"  
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        updated_data = {'display': request.POST.get('display'), 'id':id}
+        headers = {"Content-Type": "application/fhir+json"}
+        print(updated_data)
+        condition_json = json.dumps(updated_data)
+        response = requests.put(url + "/" + str(id), headers=headers, data=condition_json)
+        temp_data = response.json()
+        print(temp_data)
+        if response.status_code == 200:
+            print("condition resource created successfully")
+        else:
+            print("Error creating  resource: ")
     return render(request, "edit_data.html")
 
 
 def DeletePageView(request):
+    url = "http://hapi.fhir.org/baseR4/Condition"  
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        response =requests.delete(url + "/" + str(id))
+        temp_data = response.json()
+        print(temp_data)  
+        if response.status_code == 204:
+            print('Resource deleted successfully.')
+        else:
+            print('Error deleting resource: ', response.status_code, response.reason)      
     return render(request, "delete_data.html")
